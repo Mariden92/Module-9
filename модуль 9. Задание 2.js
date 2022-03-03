a14f7714d2fe7a3541ba1d1dd7e32e61e687d485
Mariden92 <92917668+Mariden92@users.noreply.github.com>
2022-03-03 21:04:21 +0300

    Add files via upload

---
+++ модуль 9. Задание 2.js
@@ -0,0 +1,17 @@
+const jsonStr = `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`
+   const parse = JSON.parse (jsonStr);
+   console.log (parse)
+   
